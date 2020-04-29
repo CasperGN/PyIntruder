0a704fd2029bfa5c9ccdc5d0c27c21ac0a6f2e8f
Casper <casper_too@hotmail.com>
2020-04-29 10:35:48 +0200

    Added setup.py to allow pip3 install from github

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name = 'PyIntruder',
+        version = '0.1.0',
+        author = '@sirpsycho',
+        description = 'Revised by @CasperGN - https://github.com/CasperGN/PyIntruder',
+        url = 'https://github.com/sirpsycho/PyIntruder',
+        install_requires=['requests'],
+        classifiers = [
+            'Programming Language :: Python :: 3',
+        ],
+        python_requires = '>=3.4',
+)
